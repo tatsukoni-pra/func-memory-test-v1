56279ff23ddfb72710bfde445ae2d823aebb73f0
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2024-09-29 00:11:00 +0900

    feat

--- HttpTrigger1/index.ts
+++ HttpTrigger1/index.ts
@@ -2,22 +2,40 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
+    context.log('HTTP trigger start. Memory usage:', memoryCheck(context));
 
     if (req.query.functionmode == "exit") {
         context.log('Exiting function');
         process.exit(0);
     }
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const result = consume5MB();
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: "200 OK"
     };
-
+    context.log('HTTP trigger finish. Memory usage:', memoryCheck(context));
 };
+
+function consume5MB(): string {
+    const targetSize = 5 * 1024 * 1024 / 2; // 5MB in characters
+
+    let largeString = '';
+    for (let i = 0; i < targetSize; i++) {
+        largeString += 'A';
+    }
+
+    return largeString;
+}
+
+function memoryCheck(context: Context): string {
+    const heap = process.memoryUsage();
+    const msg = [];
+    for (const key in heap) {
+        msg.push(`${key}: ${Math.round(heap[key] / 1024 / 1024)} MB`);
+    }
+    return msg.join(', ');
+}
 
 export default httpTrigger;
