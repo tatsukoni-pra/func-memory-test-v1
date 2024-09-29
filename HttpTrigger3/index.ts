c9764cb8a2470548be6f0d23e61c1c859d42a620
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2024-09-29 20:36:01 +0900

    feat

--- HttpTrigger3/index.ts
+++ HttpTrigger3/index.ts
@@ -4,7 +4,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     context.log('HTTP trigger3 start. Memory usage:', memoryCheck());
 
     var result = [];
-    for (var i = 1; i <= 10000; i++) {
+    for (var i = 1; i <= 1000000; i++) {
         result.push(Math.random().toString());
     }
     result = [];
